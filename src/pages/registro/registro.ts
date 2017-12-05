cf1464892aba3bcdba2599ffb9d522f8b0241493
MaickF78 <34277586+MaickF78@users.noreply.github.com>
2017-12-05 11:26:54 -0400

    Add files via upload

--- src/pages/registro/registro.ts
+++ src/pages/registro/registro.ts
@@ -32,15 +32,4 @@ export class RegistroPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegistroPage');
   }
-  openLink(){
-    this.iab.create("https://www.facebook.com", );
-  }
-  loginWithFB() {
-    this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
-      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
-        this.userData = {email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name']}
-      });
-    });
-  }
-
 }
